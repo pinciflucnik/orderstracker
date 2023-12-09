6e5b21492cf72f5d78be36985f33dba06dbb2bc2
Vladimir Dimitrov <133113965+pinciflucnik@users.noreply.github.com>
2023-12-09 13:32:33 +0200

    Update userData.js

--- userData.js
+++ userData.js
@@ -1,5 +1,5 @@
 export const userData = {
-    getData: () => JSON.parse(localStorage.getItem('user')),
-    setData: (data) => localStorage.setItem('user',JSON.stringify(data)),
-    clearData: () => localStorage.removeItem('user'),
+    getData: () => JSON.parse(sessionStorage.getItem('user')),
+    setData: (data) => sessionStorage.setItem('user',JSON.stringify(data)),
+    clearData: () => sessionStorage.removeItem('user'),
 }
