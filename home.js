3ab75238c34bb36281edb0f568919f392b368b60
Vladimir Dimitrov <133113965+pinciflucnik@users.noreply.github.com>
2023-12-09 13:09:52 +0200

    Update home.js

--- home.js
+++ home.js
@@ -5,7 +5,6 @@ const homeTemp = () => html`
             <div>
                 <h1>Orders Tracker</h1>
                 <p class="intro">Welcome to Orders Tracker made exclusively for personal use and training</p>
-                <span class="sign"><p>made by Pinci</p></span>
             </div>
 
 `
