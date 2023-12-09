df5592b619b9b03625fc242f8fe9dc9adf49f4be
Vladimir Dimitrov <133113965+pinciflucnik@users.noreply.github.com>
2023-12-09 13:15:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -56,8 +56,8 @@ function navUpdate(ctx, next){
     `
     const userTemp = () => html`
             <div class="navbar-nav">
-                <a class="nav-link" href="/orders">Списък с незавършени поръчки</a>
                 <a class="nav-link" href="/addOrder">Нова поръчка</a>
+                <a class="nav-link" href="/orders">Списък с незавършени поръчки</a>
                 <a class="nav-link" href="/completed">Списък със завършени поръчки</a>
                 <a class="nav-link" href="/logout">Излез</a>
             </div>
